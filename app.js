0cb362d59f914344e01d8250801fd67d1bdb5f2a
Rodion Ross <rodionross@Rodions-MacBook-Pro-2.local>
2022-09-05 11:32:32 +0100

    completed the setup

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require("express");
+
+app.use(express.json());
+
+module.exports = app;
---
+++ listen.js
@@ -0,0 +1,7 @@
+const app = require("./app.js");
+
+const PORT = 9090;
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}...`);
+});
